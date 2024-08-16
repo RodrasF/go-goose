fdff20ddbebac50ff0e2785055849c241636d239
DiogoRoq <92366120+DiogoRoq@users.noreply.github.com>
2024-08-16 23:50:09 +0100

    Feat/#8/release-goose (#13)
    * refactor: :recycle: changed path parameter names to be clearer
    
    * feat: :sparkles: added release a goose

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 // Goose identifies the structure of a Goose that will be stored in the database table Gooses
@@ -23,11 +24,15 @@ type user struct {
 	password string
 }
 
-// UserGoose identifies the structure of a goose that belongs to a user and will be
-// stored in the database table UserGooses
-type userGoose struct {
+type userGooseAssociation struct {
+	Id      string
 	UserId  int
 	GooseId int
+}
+type userGooseDto struct {
+	AssociationId string
+	Id            int
+	Name          string
 }
 
 var gooses = []goose{
@@ -40,16 +45,17 @@ var users = []user{
 	{Id: 2, Username: "RocasPT", password: "sporting"},
 }
 
-var userGooseAssociations = []userGoose{}
+var userGooseAssociations = []userGooseAssociation{}
 
 func main() {
 	router := gin.Default()
 	router.GET("/users", getUsers)
-	router.GET("/users/:id", getUser)
+	router.GET("/users/:userId", getUser)
 	router.GET("/gooses", getGooses)
 	router.GET("/catch-goose", catchGoose)
-	router.GET("/users/:id/gooses", getUserGooses)
-	router.GET("/gooses/:id", getGoose)
+	router.GET("/users/:userId/gooses", getUserGeese)
+	router.GET("/gooses/:gooseId", getGoose)
+	router.DELETE("/users/:userId/gooses/:associationId", releaseGoose)
 	router.Run("localhost:8080")
 
 }
@@ -60,7 +66,7 @@ func getUsers(c *gin.Context) {
 }
 
 func getUser(c *gin.Context) {
-	stringId := c.Param("id")
+	stringId := c.Param("userId")
 	id, err := strconv.Atoi(stringId)
 
 	if err != nil {
@@ -74,7 +80,7 @@ func getUser(c *gin.Context) {
 }
 
 func getGoose(c *gin.Context) {
-	stringId := c.Param("id")
+	stringId := c.Param("gooseId")
 	id, err := strconv.Atoi(stringId)
 
 	if err != nil {
@@ -82,12 +88,12 @@ func getGoose(c *gin.Context) {
 		c.AbortWithStatus(http.StatusNotFound)
 		return
 	}
-	var gooseIndex = slices.IndexFunc(gooses, func(goose goose) bool {return goose.Id == id})
-	if  gooseIndex == -1 {
+	var gooseIndex = slices.IndexFunc(gooses, func(goose goose) bool { return goose.Id == id })
+	if gooseIndex == -1 {
 		c.AbortWithStatus(http.StatusNotFound)
 		return
 	}
-	
+
 	c.IndentedJSON(http.StatusOK, gooses[gooseIndex])
 }
 
@@ -99,29 +105,55 @@ func catchGoose(c *gin.Context) {
 	userId := 1
 	randomGooseIndex := rand.Intn(len(gooses))
 	chosenGoose := gooses[randomGooseIndex]
-	userGoose := userGoose{userId, chosenGoose.Id}
+	userGoose := userGooseAssociation{uuid.NewString(), userId, chosenGoose.Id}
 	userGooseAssociations = append(userGooseAssociations, userGoose)
 	c.IndentedJSON(http.StatusOK, chosenGoose)
 }
 
-func getUserGooses(c *gin.Context) {
-	stringId := c.Param("id")
-	userId, err := strconv.Atoi(stringId)
-
+func releaseGoose(c *gin.Context) {
+	userId, err := paramToInt(c, "userId")
 	if err != nil {
-		fmt.Printf("%s not valid", stringId)
 		c.AbortWithStatus(http.StatusNotFound)
 		return
 	}
 
-	geeseAssociationsForUserId := filter(userGooseAssociations, func(gooseLink userGoose) bool {
-		return gooseLink.UserId == userId
+	associationId := c.Param("associationId")
+
+	gooseAssociationIndex := slices.IndexFunc(userGooseAssociations, func(userGooseAssociation userGooseAssociation) bool {
+		return userGooseAssociation.UserId == userId && userGooseAssociation.Id == associationId
+	})
+	if gooseAssociationIndex == -1 {
+		c.Status(http.StatusNoContent)
+		return
+	}
+
+	userGooseAssociations = slices.Delete(userGooseAssociations, gooseAssociationIndex, gooseAssociationIndex+1)
+	c.Status(http.StatusNoContent)
+}
+
+func getUserGeese(c *gin.Context) {
+	userId, err := paramToInt(c, "userId")
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+
+	geeseAssociationsForUserId := filter(userGooseAssociations, func(association userGooseAssociation) bool {
+		return association.UserId == userId
 	})
 
-	var userGeese = transform(geeseAssociationsForUserId, func(link userGoose) goose {
-		gooseIndex := slices.IndexFunc(gooses, func(goose goose) bool { return link.GooseId == goose.Id })
-		return gooses[gooseIndex]
+	var userGeese = transform(geeseAssociationsForUserId, func(association userGooseAssociation) userGooseDto {
+		gooseIndex := slices.IndexFunc(gooses, func(goose goose) bool { return association.GooseId == goose.Id })
+		userGoose := gooses[gooseIndex]
+		var gooseDto = userGooseDto{
+			AssociationId: association.Id,
+			Id:            userGoose.Id,
+			Name:          userGoose.Name,
+		}
+
+		return gooseDto
 	})
+
 	c.IndentedJSON(http.StatusOK, userGeese)
 }
 
@@ -141,4 +173,15 @@ func filter[T any](slice []T, funFunction func(T) bool) []T {
 		}
 	}
 	return newSlice
+}
+
+func paramToInt(c *gin.Context, urlParam string) (int, error) {
+	stringParam := c.Param(urlParam)
+	intParam, err := strconv.Atoi(stringParam)
+
+	if err != nil {
+		fmt.Printf("parameter '%s' with value '%s' not valid", urlParam, stringParam)
+		return 0, err
+	}
+	return intParam, nil
 }
